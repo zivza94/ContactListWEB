8c8ef71ea26a8c968205798c2dcddd9203bd82aa
zivza94 <zivza94@gmail.com>
2020-07-18 18:37:13 +0300

    login error msg

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -33,8 +33,8 @@ export class LoginComponent implements OnInit {
     )
     this.loginForm = new FormGroup(
       {
-        username: new FormControl(),
-        password: new FormControl()
+        username: new FormControl("admin"),
+        password: new FormControl("admin")
       }
     )
     
